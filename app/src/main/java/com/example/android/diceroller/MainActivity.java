2b7a65689b0ed6c02b71ec05598289208943f6d1
gloria61 <ozogizabella@gmail.com>
2019-11-09 19:02:59 +0000

    Part 1 Level 1 - 1838267

--- app/src/main/java/com/example/android/diceroller/MainActivity.java
+++ app/src/main/java/com/example/android/diceroller/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.android.diceroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imgview;
+    private Button button;
+
+    private int current_image;
+    int[] images = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3,
+    R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgview = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+
+        buttonClick();
     }
+
+    public void buttonClick(){
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random number = new Random();
+                current_image = number.nextInt(6)+1;
+                imgview.setImageResource(images[current_image]);
+
+            }
+        });
+
+    }
+
+
+
 }
